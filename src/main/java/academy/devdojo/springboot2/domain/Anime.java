6938c64b3ecf34f848dbc348e7a439771fb655c4
Rafael Roani Gonçalves <rafaelgoncalvesroani@gmail.com>
2024-03-06 14:15:49 -0300

    test

---
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -0,0 +1,19 @@
+package academy.devdojo.springboot2.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    private String name;
+}
